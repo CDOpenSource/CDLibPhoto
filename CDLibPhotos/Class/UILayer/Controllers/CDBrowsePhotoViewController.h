ea1bcd550fbd3e9794372ce3e3be943d436db44c
Cindy <Cindy@admindeMacBook-Pro-2.local>
2017-01-23 17:54:45 +0800

    提交代码

--- CDLibPhotos/Class/UILayer/Controllers/CDBrowsePhotoViewController.h
+++ CDLibPhotos/Class/UILayer/Controllers/CDBrowsePhotoViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+@class CDPhotoAsset;
 
 @interface CDBrowsePhotoViewController : UIViewController
 
--- CDLibPhotos/Class/UILayer/Controllers/CDBrowsePhotoViewController.m
+++ CDLibPhotos/Class/UILayer/Controllers/CDBrowsePhotoViewController.m
@@ -7,14 +7,13 @@
 //
 
 #import "CDBrowsePhotoViewController.h"
-#import "SDCycleScrollView.h"
 #import "CDPhotoAsset.h"
 
 
 
 @interface CDBrowsePhotoViewController ()
 @property (nonatomic,strong) NSArray <CDPhotoAsset *> *photoList;
-@property (nonatomic,strong) SDCycleScrollView *browseView;
+//@property (nonatomic,strong) SDCycleScrollView *browseView;
 @end
 
 @implementation CDBrowsePhotoViewController
@@ -36,38 +35,38 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    [self.browseView setBannerImageViewContentMode:UIViewContentModeScaleAspectFit];
-    [self.browseView clearCache];
-    [self.browseView setImageURLStringsGroup:self.photoList];
+//    [self.browseView setBannerImageViewContentMode:UIViewContentModeScaleAspectFit];
+//    [self.browseView clearCache];
+//    [self.browseView setImageURLStringsGroup:self.photoList];
 }
 
 #pragma mark - Getter Method
-- (SDCycleScrollView *)browseView
-{
-    if (_browseView == nil) {
-        _browseView = [SDCycleScrollView cycleScrollViewWithFrame:self.view.bounds imageURLStringsGroup:self.photoList];
-        _browseView.pageControlStyle = SDCycleScrollViewPageContolStyleAnimated;
-        _browseView.scrollDirection = UICollectionViewScrollDirectionHorizontal;
-        _browseView.placeholderImage = [UIImage imageNamed:@"picture.png"];
-        _browseView.hidesForSinglePage = YES;
-        _browseView.currentPageDotColor = [UIColor whiteColor]; // 自定义分页控件小圆标颜色
-        _browseView.pageDotColor = [UIColor lightGrayColor];
-        _browseView.pageControlDotSize = CGSizeMake(7.0, 7.0);
-        _browseView.autoScrollTimeInterval = 5.0;
-        if ([self.photoList count] <= 1) {
-            _browseView.autoScroll = NO;
-        } else {
-            _browseView.autoScroll = YES;
-        }
-        [self.view addSubview:_browseView];
-        [_browseView mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.left.equalTo(self.view);
-            make.right.equalTo(self.view);
-            make.top.equalTo(self.view);
-            make.bottom.equalTo(self.view);
-        }];
-    }
-    return _browseView;
-}
+//- (SDCycleScrollView *)browseView
+//{
+//    if (_browseView == nil) {
+//        _browseView = [SDCycleScrollView cycleScrollViewWithFrame:self.view.bounds imageURLStringsGroup:self.photoList];
+//        _browseView.pageControlStyle = SDCycleScrollViewPageContolStyleAnimated;
+//        _browseView.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+//        _browseView.placeholderImage = [UIImage imageNamed:@"picture.png"];
+//        _browseView.hidesForSinglePage = YES;
+//        _browseView.currentPageDotColor = [UIColor whiteColor]; // 自定义分页控件小圆标颜色
+//        _browseView.pageDotColor = [UIColor lightGrayColor];
+//        _browseView.pageControlDotSize = CGSizeMake(7.0, 7.0);
+//        _browseView.autoScrollTimeInterval = 5.0;
+//        if ([self.photoList count] <= 1) {
+//            _browseView.autoScroll = NO;
+//        } else {
+//            _browseView.autoScroll = YES;
+//        }
+//        [self.view addSubview:_browseView];
+//        [_browseView mas_makeConstraints:^(MASConstraintMaker *make) {
+//            make.left.equalTo(self.view);
+//            make.right.equalTo(self.view);
+//            make.top.equalTo(self.view);
+//            make.bottom.equalTo(self.view);
+//        }];
+//    }
+//    return _browseView;
+//}
 
 @end
